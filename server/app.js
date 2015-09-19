e5121ca6f922def576ee37557c6f908b440c1495
Ikuo Terado <eqobar@gmail.com>
2015-09-19 23:07:17 +0900

    Minor change

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ var url = require('url');
 
 var pubsub = require('./client').pubsub;
 
-console.log(process.env.PORT);
-var io = require('socket.io').listen(process.env.PORT || pubsub.PORT);
+var port = (process.env.PORT || pubsub.PORT);
+var io = require('socket.io').listen(port);
 var ioClient = require('socket.io-client');
 var clientScript = require('fs').readFileSync(__dirname + '/client.js');
 
